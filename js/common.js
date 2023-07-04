062bfd5ca6e26bd8be45e12f3ff10987550f24ae
luciano-meloclaps <meloclapsluciano@gmail.com>
2023-07-04 12:09:18 -0300

    Corregi lo que me faltaba

--- js/common.js
+++ js/common.js
@@ -1,8 +1,23 @@
+//Validaciones
+const FAVORITOS = [];
 
+function agregarPeliculaFavoritos(codigo) {
+  // Validar que el valor ingresado es numérico
+  if (isNaN(codigo)) {
+    console.log("Error: La pelicula seleccionada no se encientra en la API o se produjo un error al consultar");
+    return;
 
+  } else {
+    // Validar que la película no haya sido ingresada
+    if (FAVORITOS.includes(codigo)) {
+      console.log("La Pelicula ingresada ya se encuentra almacenada");
+      return;
+    }
 
-
-
-
-
-
+    // Validar la existencia del código y no repetir el mismo en el array
+    // Aquí debería ir una consulta a la base de datos para verificar si la película existe
+    // Por simplicidad, asumiremos que la película existe y se puede agregar a favoritos
+    FAVORITOS.push(codigo);
+    console.log("Película agregada con éxito");
+  }
+}
